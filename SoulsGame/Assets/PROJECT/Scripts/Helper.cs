acb385f0e1d35ad1eb7827183a6883728e5d1b1e
Danish <sim.singh993@gmail.com>
2019-06-27 11:04:24 -0400

    Animations update

---
+++ SoulsGame/Assets/PROJECT/Scripts/Helper.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float vertical;
+
+
+    public bool playAnim;
+    public string[] oh_attacks;
+    public string[] th_attacks;
+
+
+    public bool twoHanded;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("TwoHanded", twoHanded);
+
+        test();
+
+        anim.SetFloat("Vertical", vertical);
+    }
+
+    void test()
+    {
+        if (playAnim)
+        {
+            string targetAnim;
+
+            if (twoHanded)
+            {
+                int r = Random.Range(0, th_attacks.Length);
+                targetAnim = th_attacks[r];
+            }
+            else
+            {
+                int r = Random.Range(0, oh_attacks.Length);
+                targetAnim = oh_attacks[r];
+            }
+            vertical = 0;
+            anim.CrossFade(targetAnim, 0.2f);
+            playAnim = false;
+        }
+    }
+}
